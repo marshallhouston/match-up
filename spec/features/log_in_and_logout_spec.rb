2a33aa0fa6e4e7b34f4ad7c97937dd9f85f14e80
Marshall Houston <marshall.houston@gmail.com>
2018-01-04 15:59:15 -0700

    create test for logging in with google oauth

---
+++ spec/features/log_in_and_logout_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "As a guest" do
+  scenario "I can login using google account information and then logout" do
+    visit root_path
+    stub_omniauth
+
+    click_on "Login with Google"
+
+    expect(page).to have_link("Logout")
+
+    click_on "Logout"
+
+    expect(page).to have_link("Login with Google")
+    expect(page).to_not have_link("Logout")
+  end
+end
