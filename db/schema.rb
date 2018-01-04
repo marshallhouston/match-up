dba8134ff595e1546f78880ee1d6aa5fc0005949
Marshall Houston <marshall.houston@gmail.com>
2018-01-04 15:33:01 -0700

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103204624) do
+ActiveRecord::Schema.define(version: 20180104060309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20180103204624) do
   create_table "schools", force: :cascade do |t|
     t.string "name"
     t.integer "min_admission_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "email"
+    t.string "first_name"
+    t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
