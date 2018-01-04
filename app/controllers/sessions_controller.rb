f5f10076a058f6c274b4dcc2275b8d8e348a633b
Marshall Houston <marshall.houston@gmail.com>
2018-01-04 16:01:17 -0700

    update controllers for logging in with current_user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def create
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+      redirect_to new_admission_score_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+end
