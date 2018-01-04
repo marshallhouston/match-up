24b413453969cdfed183d42bdfaa31e1531d9d58
Marshall Houston <marshall.houston@gmail.com>
2018-01-03 20:42:06 -0700

    change sequence in schools factory for failing circleCI build

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :school do
-    sequence :name { |n| "School#{n}"}
+    sequence :name do |n|
+      "School#{n}"
+    end
     min_admission_score { [45..146].sample}
   end
 end
