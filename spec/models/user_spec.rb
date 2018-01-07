45787cb59af4e2a35e87091fd77c8a9632dfd75c
Marshall Houston <marshall.houston@gmail.com>
2018-01-07 10:44:25 -0700

    add test for user having many admission scores

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "associations" do
+    it "has many admission_scores" do
+      user = create(:user)
+
+      expect(user).to respond_to(:admission_scores)
+    end
+  end
 end
