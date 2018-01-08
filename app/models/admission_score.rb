4accdaf3bb40a9e224684c36504e4dcede13fbf4
Marshall Houston <marshall.houston@gmail.com>
2018-01-07 18:35:37 -0700

    add relationships with school_scores

--- app/models/admission_score.rb
+++ app/models/admission_score.rb
@@ -2,21 +2,23 @@ class AdmissionScore < ApplicationRecord
   validates :gpa, presence: true, numericality: { less_than_or_equal_to: 4.0 }
   validates :sat_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 400, less_than_or_equal_to: 1600 }
   belongs_to :user
+  has_many :score_schools
+  has_many :schools, through: :score_schools
 
   def self.latest_scores
     order(created_at: :desc)
   end
 
   def strong_schools
-    School.where('? >= min_admission_score', (admission_index_score - 10)).order(min_admission_score: :desc)
+    schools.where(score_schools: {status: 'strong'} )
   end
 
   def average_schools
-    School.where(min_admission_score: (admission_index_score - 10)..admission_index_score).order(:min_admission_score).reverse
+    schools.where(score_schools: {status: 'avg'} )
   end
 
   def below_average_schools
-    School.where('min_admission_score > ?', admission_index_score).order(:min_admission_score)
+    schools.where(score_schools: {status: 'below_avg'} )
   end
 
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,11 @@
 class School < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :min_admission_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 45, less_than_or_equal_to: 146 }
+  has_many :score_schools
+  has_many :admission_scores, through: :score_schools
+
+  def self.filter_by(status)
+    where(score_schools: {status: status})
+  end
+
 end
