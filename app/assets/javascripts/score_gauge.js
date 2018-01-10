f30b86d5a532ab08cd23af15406f77806d2ae2f6
Marshall Houston <marshall.houston@gmail.com>
2018-01-09 18:59:38 -0700

    move js for socre gauge to separate file

---
+++ app/assets/javascripts/score_gauge.js
@@ -0,0 +1,32 @@
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var admissionScore = $('#admissionScore').html();
+
+  var data1 = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Your Score', 0]
+  ]);
+
+  var data2 = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Your Score', parseInt(admissionScore)]
+  ]);
+
+  var options = {
+    width: 400, height: 120,
+    redFrom: 45, redTo: 75,
+    yellowFrom:75, yellowTo: 90,
+    greenFrom:90, greenTo: 146,
+    minorTicks: 10,
+    min: 45,
+    max: 146
+  };
+
+  var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+  chart.draw(data1, options);
+  chart.draw(data2, options);
+}
