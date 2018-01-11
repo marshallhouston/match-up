39c1f0cc1dca200e9012a0b3950dfd47e0cbad4b
Marshall Houston <marshall.houston@gmail.com>
2018-01-11 15:02:37 -0700

    add initial handling for avg/below_avg/strong

--- app/controllers/score_schools_controller.rb
+++ app/controllers/score_schools_controller.rb
@@ -3,7 +3,7 @@ class ScoreSchoolsController < ApplicationController
   before_action :logged_in?, only: :index
 
   def index
-    @status = school_score_params[:status]
+    @status = status_display[school_score_params[:status]]
     @admission_score = admission_score
     @schools = admission_score.schools.filter_by(school_score_params[:status])
   end
@@ -17,4 +17,12 @@ class ScoreSchoolsController < ApplicationController
     def school_score_params
       params.permit(:status, :admission_score_id)
     end
+
+    def status_display
+      base_status = Hash.new("an average")
+      valid_status = {"avg" => "an average", "below_avg" => "a below average", "strong" => "a strong"}
+      base_status.merge(valid_status)
+    end
+
+
 end
